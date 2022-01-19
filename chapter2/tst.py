448abf0d7ecea6f7b43146c96c40fbd464d84c9c
Olayinka Adebisi <adebisiolayinka30@gmail.com>
2022-01-19 01:27:31 +0100

    updated

--- chapter2/tst.py
+++ chapter2/tst.py
@@ -22,21 +22,26 @@
 # print(spam(0))
 
 #This is a guess the number game
-import random
-secretnumber = random.randint(1,20)
-print('I am thinking of a number between 1 and 20')
-for numofguesses in range(1,7):
-    guess = int(input('Take a guess: '))
+# import random
+# secretnumber = random.randint(1,20)
+# print('I am thinking of a number between 1 and 20')
+# for numofguesses in range(1,7):
+#     guess = int(input('Take a guess: '))
 
-    if guess > secretnumber:
-        print('Your guess is too high')
-    elif guess < secretnumber:
-        print('Your guess is too low')
-    else:
-        break
-if guess == secretnumber:
-    print('Good job! You guessed my number in ' + str(numofguesses) + ' guesses!')
-else:
-    print('Nope! my secret number is ' + str(secretnumber))
-    print('Nope. The number I was thinking of was ' + str(secretnumber))
+#     if guess > secretnumber:
+#         print('Your guess is too high')
+#     elif guess < secretnumber:
+#         print('Your guess is too low')
+#     else:
+#         break
+# if guess == secretnumber:
+#     print('Good job! You guessed my number in ' + str(numofguesses) + ' guesses!')
+# else:
+#     print('Nope! my secret number is ' + str(secretnumber))
+#     print('Nope. The number I was thinking of was ' + str(secretnumber))
+    
+def addnum(x,y):
+    return x+y
+    
 
+addnum(5,1827377)
--- chapter3/task2.py
+++ chapter3/task2.py
@@ -1,1 +1,28 @@
 #A function is like a mini-function inside a program
+# # Write a function named collatz() that has one parameter named number. If 
+# number is even, then collatz() should print number // 2 and return this value. 
+# If number is odd, then collatz() should print and return 3 * number + 1.
+# Then write a program that lets the user type in an integer and that keeps 
+# calling collatz() on that number until the function returns the value 1
+
+# from ast import Num
+
+
+def collatz(number):
+    if number % 2 == 0:
+        return number // 2
+        
+    elif number % 2 ==1:
+        return number * 3 + 1
+while True:
+    yinka = int(input('Input a number: '))
+    print(collatz(yinka))
+    if collatz(yinka) != 1:
+        continue
+    elif yinka == int:
+        print('please input an integer')
+        
+    else:
+        break
+
+       
