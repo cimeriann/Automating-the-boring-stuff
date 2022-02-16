526ab5e6dc97394fc95233a8be2c5fd09200b24a
Olayinka Adebisi <adebisiolayinka30@gmail.com>
2022-02-16 10:08:45 +0100

    added a new file

---
+++ chapter 5/countcharacters.py
@@ -0,0 +1,8 @@
+message = 'The world is a very beautiful place. The current month is February'
+
+count = {}
+for character in message:
+    count.setdefault(character, 0)
+    count[character] = count[character] + 1
+
+print(count)
--- chapter 5/tasks4.py
+++ chapter 5/tasks4.py
@@ -1,2 +1,6 @@
 birthdays = {'dami': 'April the 5th'}
 print(birthdays['dami'])
+picnic_items = {'apples': 2, 'cups': 4}
+print('I am bringing ' + str(picnic_items.get('mangoes', 0)) + ' apples')
+picnic_items.setdefault('blankets', 5)
+print(picnic_items)
