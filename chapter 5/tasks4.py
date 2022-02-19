b6f9468cac003cdb0bb2d89771d5c3adc3ac7b0f
Olayinka Adebisi <adebisiolayinka30@gmail.com>
2022-02-19 09:06:36 +0100

    solutions to chapter 5

--- chapter 5/tasks4.py
+++ chapter 5/tasks4.py
@@ -1,3 +1,4 @@
+# display inventory code
 inventory = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
 def display_inventory(some_inventory):
     print('Inventory: ')
@@ -10,6 +11,12 @@ def display_inventory(some_inventory):
     print('Total number of items: ' + str(total))
 
 display_inventory(inventory)
+
+# add to inventory code
+# dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+# def add_to_inventory(some_inventory, list_of_items):
+
+    
 
 
     
