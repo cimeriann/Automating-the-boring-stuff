7777aabe6039321a758f487333d9f36a918f5be1
Olayinka Adebisi <adebisiolayinka30@gmail.com>
2022-02-19 08:03:15 +0100

    solutions to chapter 5

--- chapter 5/tasks4.py
+++ chapter 5/tasks4.py
@@ -1,19 +1,15 @@
-birthdays = {'dami': 'April the 5th'}
-print(birthdays['dami'])
-picnic_items = {'apples': 2, 'cups': 4}
-print('I am bringing ' + str(picnic_items.get('mangoes', 0)) + ' apples')
-picnic_items.setdefault('blankets', 5)
-print(picnic_items)
+inventory = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
+def display_inventory(some_inventory):
+    print('Inventory: ')
+    for k,v in some_inventory.items():
+        print(str(v)+ ' ' + k)
+    total = 0
+    for k,v in some_inventory.items():
+        total += v
 
-# A simple tic tac toe game using dictionaries
-theboard = { 'top-L': ' ', 'top-M': ' ', 'top-R': ' ',
-             'mid-L': ' ', 'mid-M': ' ', 'mid-R': ' ',
-             'low-L': ' ', 'low-M': ' ', 'low-R': ' ' }
+    print('Total number of items: ' + str(total))
 
-def printBoard(board):
-    print(theboard['top-L'] + '|' + theboard['top-M'] + '|' + theboard['top-R'])
-    print('-+-+-')
-    print(theboard['mid-L'] + '|' + theboard['mid-M'] + '|' + theboard['mid-R'])
-    print('-+-+-')
-    print(theboard['low-L'] + '|' + theboard['low-M'] + '|' + theboard['low-R'])
-printBoard(theboard)
+display_inventory(inventory)
+
+
+    
